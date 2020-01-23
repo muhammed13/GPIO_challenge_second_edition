7a868a63012072d9e75b46748429078d43868ded
muhammed13 <40000647+muhammed13@users.noreply.github.com>
2020-01-23 23:33:19 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,40 +3,7 @@
 #include "GPIO_REQ9.h"
 #include "timers.h"
 #include "softwareDelay.h"
-#include <avr/interrupt.h>
-//#include "interrupt.h"
-
-
-ISR(INT0_vect)
-{
-	gpioPinToggle(GPIOB, BIT1);
-}
-
-void INT0_Init(void)
-{
-	SREG  &= ~(1<<7);                   // Disable interrupts by clearing I-bit
-	//DDRD  &= (~(1<<PD2));               // Configure INT0/PD2 as input pin
-	GICR  |= (1<<INT0);                 // Enable external interrupt pin INT0
-	MCUCR |= (1<<ISC00) | (1<<ISC01);   // Trigger INT0 with the raising edge
-	SREG  |= (1<<7);                    // Enable interrupts by setting I-bit
-}
-
-/*
-ISR_(INT0_vect)
-{
-	gpioPinToggle(GPIOB, BIT1);
-}
-
-void INT0_Init(void)
-{
-	sei_();                   // Disable interrupts by clearing I-bit
-	//DDRD  &= (~(1<<PD2));               // Configure INT0/PD2 as input pin
-	GICR  |= (1<<INT0);                 // Enable external interrupt pin INT0
-	MCUCR |= (1<<ISC00) | (1<<ISC01);   // Trigger INT0 with the raising edge
-	cli_();                   // Enable interrupts by setting I-bit
-}
-*/
-
+#include "interrupt.h"
 int main()
 {
 	/*REQ7*/
